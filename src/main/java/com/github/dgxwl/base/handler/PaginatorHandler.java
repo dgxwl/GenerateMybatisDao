61b4f8bebf01b01b86b4b0193d4fe00fe9f963b1
dgxwl <1029325214@qq.com>
2021-06-29 14:59:51 +0800

    分页导入优化

--- src/main/java/com/github/dgxwl/base/handler/PaginatorHandler.java
+++ src/main/java/com/github/dgxwl/base/handler/PaginatorHandler.java
@@ -5,15 +5,24 @@ import java.util.Map;
 
 public class PaginatorHandler {
 
-    private static Map<String, String> importMap = new HashMap<>();
+	private static Map<String, String> serviceImportMap = new HashMap<>(4);
+    private static Map<String, String> mapperImportMap = new HashMap<>(4);
     static {
-        importMap.put("pageHelper", "import com.github.pagehelper.PageHelper;\n" +
-                                    "import com.github.pagehelper.Page;\n");
-        importMap.put("mybatis-paginator", "import com.github.miemiedev.mybatis.paginator.domain.PageBounds;\n" +
+    	serviceImportMap.put("pageHelper", "import com.github.pagehelper.PageHelper;\n" +
+                "import com.github.pagehelper.Page;\n");
+    	serviceImportMap.put("mybatis-paginator", "import com.github.miemiedev.mybatis.paginator.domain.PageBounds;\n" +
+		                       "import com.github.miemiedev.mybatis.paginator.domain.PageList;\n");
+
+        mapperImportMap.put("pageHelper", "");
+        mapperImportMap.put("mybatis-paginator", "import com.github.miemiedev.mybatis.paginator.domain.PageBounds;\n" +
                                            "import com.github.miemiedev.mybatis.paginator.domain.PageList;\n");
     }
+    
+    public static String getServiceImportStr(String paginator) {
+        return serviceImportMap.get(paginator);
+    }
 
-    public static String getImports(String paginator) {
-        return importMap.get(paginator);
+    public static String getMapperImportStr(String paginator) {
+        return mapperImportMap.get(paginator);
     }
 }
